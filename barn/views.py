128b560db5e6f6dab94b0e7aaf7bd1c66c5403e3
said <saidorifx@gmail.com>
2025-08-22 16:15:24 +0500

    choy

--- barn/views.py
+++ barn/views.py
@@ -7,34 +7,34 @@ from .serializers import BarnSerializer
 class BarnCreateView(generics.CreateAPIView):
     queryset = Barn.objects.all()
     serializer_class = BarnSerializer
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
 
 
 class BarnRetrieveView(generics.RetrieveAPIView):
     queryset = Barn.objects.all()
     serializer_class = BarnSerializer
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
     lookup_field = 'pk'  # Now using primary key
 
 
 class BarnUpdateView(generics.UpdateAPIView):
     queryset = Barn.objects.all()
     serializer_class = BarnSerializer
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
     lookup_field = 'pk'
 
 
 class BarnDeleteView(generics.DestroyAPIView):
     queryset = Barn.objects.all()
     serializer_class = BarnSerializer
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
     lookup_field = 'pk'
 
 
 class BarnListView(generics.ListAPIView):
     queryset = Barn.objects.all()
     serializer_class = BarnSerializer
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
 
 
 # class BarnSearchAPIView(generics.ListAPIView):
